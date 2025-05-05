8d6bbaf59adf016f6d11f9c688f08867206267df
haidarfatah <haidarfatah224@gmail.com>
2025-05-05 10:33:56 +0700

    test

--- KasMin-Kasir_Mini_Market/frmDashboard.Designer.cs
+++ KasMin-Kasir_Mini_Market/frmDashboard.Designer.cs
@@ -34,7 +34,7 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(341, 282);
+            label1.Location = new Point(222, 209);
             label1.Name = "label1";
             label1.Size = new Size(59, 25);
             label1.TabIndex = 0;
